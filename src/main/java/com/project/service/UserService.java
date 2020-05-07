aacd5ec1e8dfb92f750e60cc5f528aedd8b19df6
huelton <hueltondihon@gmail.com>
2020-05-07 12:17:56 -0300

    criado ObjectNotFoundException e findById

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -1,12 +1,14 @@
 package com.project.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.domain.User;
 import com.project.repository.UserRepository;
+import com.project.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll() {
 		 return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
