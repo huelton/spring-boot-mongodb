1d1aa3b6af78c82bf6dc0dcb6631e871e12fb8e3
huelton <hueltondihon@gmail.com>
2020-05-06 23:33:57 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,27 +1,27 @@
 package com.project.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.domain.User;
+import com.project.service.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User(1L,"maria","maria@gmail.com");
-		User joao = new User(2L,"joao","joao@gmail.com");
 		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, joao));
+		List<User> list = userService.findAll();
 		
 		return ResponseEntity.ok().body(list);
 		
--- src/main/java/com/project/domain/User.java
+++ src/main/java/com/project/domain/User.java
@@ -2,6 +2,9 @@ package com.project.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -17,13 +20,17 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Document(collection = "user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private Long id;
+	@Id
+	private String id;
 	private String nome;
 	private String email;
+	
+	
 	
 	
 }
